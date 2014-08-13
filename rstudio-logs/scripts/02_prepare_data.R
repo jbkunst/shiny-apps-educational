f0ca88c9646022d351eb3265fe2a4fe3e1df7dd5
Joshua <jbkunst@gmail.com>
2014-08-13 18:52:06 -0400

    the algorithm was fine, the logs on 2012 show weird dates.

--- rstudio-logs/scripts/02_prepare_data.R
+++ rstudio-logs/scripts/02_prepare_data.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(plyr)
+library(stringr)
 library(RSQLite)
 options(stringsAsFactors = FALSE)
 source("00_parameters.R")
@@ -7,25 +8,68 @@ source("00_parameters.R")
 
 suppressWarnings(dir.create(pars$folder_data_app))
 
+
 files_compress <- dir(path = pars$folder_data_raw, full.names = TRUE)
 files_compress <- files_compress[file.info(files_compress)$size != 0]
+files_compress <- files_compress[!grepl("2012-\\d{2}-\\d{2}.csv.gz$", files_compress)]
 
-data <- ldply(files_compress, function(x){  
-  subset(read.table(gzfile(x), nrows =  -1, sep = ",", quote = "\"", header = TRUE), select = pars$logs_col_names)
+# try(file.remove(file.path(pars$folder_data_app, pars$file_sqlite_name)))
+db <- dbConnect(SQLite(), dbname = file.path(pars$folder_data_app, pars$file_sqlite_name))
+
+
+l_ply(files_compress, function(x){  # x <- sample(files_compress, size = 1)
+  
+  # Check if we load the data in a previous process
+  d <- subset(read.table(gzfile(x), nrows =  -1, sep = ",", quote = "\"", header = TRUE), select = pars$logs_col_names)
+  file_date <- str_extract(x, "\\d{4}-\\d{2}-\\d{2}")
+  start <- as.numeric(strptime(paste(file_date, "00:00:00"), "%Y-%m-%d %H:%M:%S"))
+  end <- as.numeric(strptime(paste(file_date, "23:59:59"), "%Y-%m-%d %H:%M:%S"))
+  
+  if(nrow(dbGetQuery(db, "SELECT name FROM sqlite_master WHERE type='table' AND name='rstudio_logs'"))==0){
+    db_nrow <- 0
+  } else {
+    db_nrow <- dbGetQuery(db, sprintf("select count(*) from rstudio_logs where timestamp >= %s and timestamp <= %s", start, end))  
+  }
+
+  if(db_nrow == 0){
+    
+    # Creating timestamp
+    d$timestamp <- as.numeric(strptime(paste(d$date, d$time), "%Y-%m-%d %H:%M:%S"))
+    # d$date_revert <- format(as.POSIXct(d$timestamp, tz = "GMT", origin = "1970-01-01"), "%Y-%m-%d")
+    # d$time_revert <- format(as.POSIXct(d$timestamp, tz = "GMT", origin = "1970-01-01"), "%H:%M:%S")
+    d <- subset(d, select = c("timestamp", "package", "country"))
+    
+    dbWriteTable(db, name = "rstudio_logs", value = d, append = TRUE)
+    message(sprintf("File %s, rows %s", x, prettyNum(nrow(d), big.mark =  ".")))
+    
+  } else {
+    message(sprintf("File %s, rows %s, rows in db %s, NOT uploaded", x, prettyNum(nrow(d), big.mark =  "."), prettyNum(db_nrow, big.mark = "."))) 
+  }
+    
 }, .progress="text")
 
-try(file.remove(file.path(pars$folder_data_app, pars$file_sqlite_name)))
-
-db <- dbConnect(SQLite(), dbname = file.path(pars$folder_data_app, pars$file_sqlite_name))
-dbWriteTable(conn = db, name = "rstudio_logs", value = data)
 
 
-
-f1 <- "2012-11-07"
-f2 <- "2013-11-10"
-head(data)
-str(data)
-dim(data)
-
-system.time(res1 <- dbGetQuery(db, sprintf("select * from rstudio_logs where date >= '%s' and date <= '%s'", f1, f2)))
-system.time(res2 <- subset(data, date >= f1 & date <= f2))
+# test <- dbGetQuery(db, "select *, datetime(timestamp, 'unixepoch', 'localtime') as datetime_timestamp_sqlite, date(timestamp, 'unixepoch', 'localtime') as date_timestamp_sqlite, time(timestamp, 'unixepoch', 'localtime') as time_timestamp_sqlite from rstudio_logs")
+# test$date_time <- format(as.POSIXct(test$timestamp, tz = "", origin = "1970-01-01"), "%Y-%m-%d %H:%M:%S")
+# test$date <- format(as.POSIXct(test$timestamp, tz = "", origin = "1970-01-01"), "%Y-%m-%d")
+# head(test)
+# str(test)
+# 
+# sum(files_counts$file_n)
+# 
+# test2 <- test[!test$date_time == test$datetime_timestamp_sqlite,]
+# head(test2)
+# 
+# sqlite_count2 <- ddply(test, .(date), summarize, sqlite2_n = length(date))
+# sqlite_count <- dbGetQuery(db, "select date(timestamp, 'unixepoch', 'localtime') as date, count(*) as sqlite_n from rstudio_logs group by date(timestamp, 'unixepoch', 'localtime')")
+# files_counts2 <- ldply(files_compress, function(x){
+#   data.frame(date = file_date <- str_extract(x, "\\d{4}-\\d{2}-\\d{2}"),
+#              files_n2 = nrow(read.table(gzfile(x), nrows =  -1, sep = ",", quote = "\"", header = TRUE)))
+# }, .progress="text")
+# 
+# test_f <- join(sqlite_count, sqlite_count2, type = "full")
+# test_f <- join(test_f, files_counts, type = "full")
+# test_f <- join(test_f, files_counts2, type = "full")
+# test_f <- test_f[order(test_f$date), ]
+# View(test_f)
