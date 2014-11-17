64c7080ce8651ab3d8b1cd84115ddd243f183d22
Joshua <jbkunst@gmail.com>
2014-11-16 22:39:23 -0300

    testing why dont work on server

--- cl-educ/ui.R
+++ cl-educ/ui.R
@@ -12,27 +12,27 @@ shinyUI(
                         p("en construccion")
                         ),
                tabPanel("REGION",
-                        selectizeInput("region_numero", NULL, regiones_choices, width = "400"),
+#                         selectizeInput("region_numero", NULL, regiones_choices, width = "400"),
                         p("en construccion")
                         ),
                tabPanel("COLEGIO",
                         column(width = 3,
-                               selectizeInput("colegio_rbd", NULL, colegios_choices, selected = 10726, width="90%"),
-                               selectizeInput("indicador", NULL, indicador_choices, width="90%"),
+#                                selectizeInput("colegio_rbd", NULL, colegios_choices, selected = 10726, width="90%"),
+#                                selectizeInput("indicador", NULL, indicador_choices, width="90%"),
                                hr(),
                                tags$span(paste(
                                  "Puedes comparar el colegio seleccionado",
                                  "considerando los colegios con caracteristicas similares:"
                                )),
                                br(),
-                               checkboxInput("colegio_misma_region", "Misma region", FALSE),
-                               checkboxInput("colegio_misma_dependencia", "Misma dependencia", FALSE),
-                               checkboxInput("colegio_misma_area", "Misma área geografica", FALSE),
+#                                checkboxInput("colegio_misma_region", "Misma region", FALSE),
+#                                checkboxInput("colegio_misma_dependencia", "Misma dependencia", FALSE),
+#                                checkboxInput("colegio_misma_area", "Misma área geografica", FALSE),
                                br()
                                ),
                         column(width = 5,
                                includeScript("www/js/hc_custom.js"), 
-                               chartOutput("plot_colegio", "highcharts"),
+#                                chartOutput("plot_colegio", "highcharts"),
                                br()
                                ),
                         column(width = 4,
