bc99d118984d09703c6b756f076734f2ebad49da
Joshua <jbkunst@gmail.com>
2015-03-24 16:05:44 -0300

    nothing

--- nasa-neo/scripts/01_test.R
+++ nasa-neo/scripts/01_test.R
@@ -1,16 +1,22 @@
-library("readr")
 library("dplyr")
 library("tidyr")
+library("readr")
+library("ggplot2")
+
+# devtools::install_github("jrnold/ggthemes")
+library("ggthemes")
 
 
 data <- read_csv("~/../Downloads/MOD15A2_M_LAI_2015-02-01_gs_250x125.SS.CSV") 
-head(data)
+names(data)[1] <- "lat"
 
+data_g <- data %>%
+  gather(lon, value, -lat) %>%
+  filter(value != 99999)
+#  mutate(value = ifelse(value == 99999, NA, value))
+  
 
-
-
-
-url2 <- "http://neo.sci.gsfc.nasa.gov/servlet/RenderData?si=1612886&cs=rgb&format=SS.CSV&width=3600&height=1800"
-data2 <- read_csv(url2)
-head(data2)
-
+ggplot(data_g) +
+  geom_point(aes(lon, lat, color = value)) +
+  theme_map()
+  
