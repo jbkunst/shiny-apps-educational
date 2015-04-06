a372a15f445a0293cfb9ffb716a3c2330f3ba6eb
Joshua <jbkunst@gmail.com>
2015-04-06 17:51:42 -0300

    ?

--- catalogue/server.R
+++ catalogue/server.R
@@ -28,7 +28,7 @@ shinyServer(function(input, output, session) {
   observe({
     values$makeorder <- input$makeorder
     if(!is.null(input$makeorder) && values$makeorder){
-      message("processing order")
+      message("processing order put your logic here, the session will be restarted")
     }
   })
 
@@ -57,7 +57,7 @@ shinyServer(function(input, output, session) {
     
     data_sort
     
-  })
+
   
   data_price <- reactive({
     
