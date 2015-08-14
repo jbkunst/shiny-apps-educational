a72830026e9896ba917cfc8252eec3757663c22e
Joshua <jbkunst@gmail.com>
2015-08-14 17:32:45 -0300

    updating d3wordcloud cause package

--- d3wordcloud/global.R
+++ d3wordcloud/global.R
@@ -3,9 +3,20 @@ library("htmlwidgets")
 library("d3wordcloud")
 library("tm")
 library("dplyr")
+library("ggthemes")
 
 load("data.RData")
 
-choices_font <- c("Open Sans", "Lato", "Raleway", "Impact", "Comic Sans MS (No plz!)" = "Comic Sans MS",
-                  "Arial", "Arial Black", "Tahoma", "Verdana", "Courier New",
+choices_font <- c("Open Sans", "Lato", "Raleway", "Comic Sans MS (No plz!)" = "Comic Sans MS",
+                  "Erica One", "Anton", "Arial", "Arial Black", "Tahoma", "Verdana", "Courier New",
                   "Georgia", "Times New Roman", "Andale Mono")
+
+colors <- c(ggthemes_data$hc$palettes$default,
+            ggthemes_data$hc$palettes$darkunica,
+            ggthemes_data$fivethirtyeight,
+            ggthemes_data$economist)
+colors <- sample(colors)
+colors <- substr(colors, 0, 7)
+
+
+
--- d3wordcloud/server.R
+++ d3wordcloud/server.R
@@ -13,8 +13,17 @@ shinyServer(function(input, output) {
       arrange(desc(freq)) %>%
       head(input$n_words)
     
-    d3wordcloud(d$word, d$freq, font = input$font, font.weight = input$font_weight,
-                scale = input$scale, padding = input$padding,
+    if (input$color_type == "a color by word") {
+      colorswc <- sample(input$colors, size = input$n_words, replace = TRUE)
+    } else {
+      colorswc <- input$colors
+    }
+      
+    d3wordcloud(d$word, d$freq, font = input$font, 
+                colors = colorswc,
+                size.scale = input$size_scale,
+                color.scale = input$color_scale,
+                padding = input$padding,
                 spiral = input$spiral,
                 rotate.min = input$rotate[1], rotate.max = input$rotate[2])
   })
