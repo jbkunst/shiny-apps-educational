d6ee50a095750dcd4ea9d60c7f9d71c60faf00fa
Joshua <jbkunst@gmail.com>
2016-01-14 14:37:44 -0300

    uploading this highcarter example to shinyapps.io

--- highcharter/server.R
+++ highcharter/server.R
@@ -9,20 +9,26 @@ function(input, output) {
       hc_add_serie(name = "London", data = citytemp$london) %>% 
       hc_add_serie(name = "New York", data = abs(citytemp$new_york))
     
-    if(input$ena) hc <- hc %>% hc_chart(options3d = list(enabled = TRUE, beta = input$beta, alpha = input$alpha))
+    if (input$ena)
+      hc <- hc %>% hc_chart(options3d = list(enabled = TRUE, beta = input$beta, alpha = input$alpha))
     
-    if(input$stacked != FALSE) hc <- hc %>% hc_plotOptions(series = list(stacking = input$stacked))
+    if (input$stacked != FALSE)
+      hc <- hc %>% hc_plotOptions(series = list(stacking = input$stacked))
     
-    if(input$theme != FALSE) {
-      theme <- switch (input$theme,
-                       darkunica = hc_theme_darkunica(),
-                       gridlight = hc_theme_gridlight(),
-                       sandsignika = hc_theme_sandsignika()
-      )
-      
+    if (input$credits)
+      hc <- hc %>% hc_credits(enabled = TRUE, text = "Highcharter", href = "http://jkunst.com/highcharter/")
+    
+    if (input$exporting)
+      hc <- hc %>% hc_exporting(enabled = TRUE)
+    
+    if (input$theme != FALSE) {
+      theme <- switch(input$theme,
+                      darkunica = hc_theme_darkunica(),
+                      gridlight = hc_theme_gridlight(),
+                      sandsignika = hc_theme_sandsignika()
+                      )
       hc <- hc %>% hc_add_theme(theme)
-      
-    }
+      }
     
     hc
     
