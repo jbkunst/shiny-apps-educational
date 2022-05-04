e27c0262500fac0cb836cd4a02acfde0547b5b64
Joshua Kunst <jbkunst@gmail.com>
2022-05-04 00:48:16 -0400

    ipdalaod

---
+++ run_an_app.R
@@ -0,0 +1,27 @@
+app_to_run <- functio(app = "kmeans"){
+  
+  url <- "https://github.com/jbkunst/shiny-apps-edu/archive/refs/heads/master.zip"
+  
+  filePath <- tempfile("shinyapp", fileext = ".zip")
+  fileDir <- tempfile("shinyapp")  
+  
+  message("Donloading ", url)
+  
+  download.file(url, filePath)
+  
+  try(utils::unzip(filePath, exdir = fileDir))
+
+  fp <- file.path(fileDir, "shiny-apps-edu-master", app)
+  
+  if(any(stringr::str_detect(dir(fp), "app.Rmd"))) {
+    app_rmd <- stringr::str_subset(dir(fp, full.names = TRUE), "app.Rmd")
+    rmarkdown::run(app_rmd)
+    
+  } else {
+    # normal shiny
+  }
+  
+  
+}
+
+# app_to_run("kmeans")
