ccad8c84167b4a9d543ab2906c4cc0bed8c9d24f
Joshua Kunst <jbkunst@gmail.com>
2022-11-29 02:38:32 -0300

    Update deploy_apps.R

--- deploy_apps.R
+++ deploy_apps.R
@@ -2,16 +2,27 @@ library(tidyverse)
 
 # remotes::install_github("jbkunst/highcharter", force = TRUE)
 # remotes::install_github("jbkunst/klassets", force = TRUE)
+# remotes::install_github("jbkunst/risk3r", force = TRUE)
 
 apps <- fs::dir_ls(here::here(), full.names = TRUE) |>
   str_subset("\\.", negate = TRUE) |> 
   str_subset("older-versions", negate = TRUE) 
 
-walk(apps, function(app = "D:/Git/shiny-apps/chess-explorer"){
+if(FALSE){
+  # delete all rsconnect folders
   
-  cli::cli_h2(app)
+  fs::dir_ls(recurse = TRUE) |> 
+    stringr::str_subset("rsconnect$") |> 
+    fs::dir_delete()
   
-  try(fs::dir_delete(fs::path(app, "rsconnect")))
+}
+
+walk(apps, function(app = "D:/Git/shiny-apps/kmeans"){
+  
+  cli::cli_h1(basename(app))
+  cli::cli_inform(app)
+  
+  if(fs::dir_exists(fs::path(app, "rsconnect"))) return(TRUE)
   
   rsconnect::deployApp(appDir = app, logLevel = "normal")
   
